75bd3627b6f9786e27a0a69f88f303660c9e7981
CM <bcy4601@gmail.com>
2026-01-14 22:19:42 +0900

    0114

--- CodeTest/Silver/1149.cpp
+++ CodeTest/Silver/1149.cpp
@@ -21,24 +21,43 @@ int main()
     ios_base::sync_with_stdio(false);
     cin.tie(0);
     cout.tie(0);
-    // scanf/printf/puts/getchar/putchar 등 C의 입출력 방식을 사용하면 안 된다.
 
-    int N{}, M{};
+    int N{};
     int iIter{};
-    int a{}, b{};
     int res{};
+    //vector<int> dp;
+    int dp[1001][3];
 
+    vector<array<int, 3>> house;
 
     cin >> N;
 
+    int r, g, b;
+
     for (size_t i = 0; i < N; i++)
     {
+        cin >> r >> g >> b;
 
+        house.push_back({ r,g,b });
     }
 
+    dp[0][0] = house.front()[0];
+    dp[0][1] = house.front()[1];
+    dp[0][2] = house.front()[2];
 
+    int lastcolor{};
+    for (int i = 1; i < N; i++)
+    {
+        dp[i][0] = min(dp[i - 1][1], dp[i - 1][2])
+            + house[i][0];
+        dp[i][1] = min(dp[i - 1][0], dp[i - 1][2])
+            + house[i][1];
+        dp[i][2] = min(dp[i - 1][1], dp[i - 1][0])
+            + house[i][2];
+    }
 
-    cout << res;
+    cout << min({dp[N - 1][0], dp[N - 1][1], dp[N - 1][2] });
+
 
     return 0;
 }
